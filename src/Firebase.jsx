5afc94a22c025cab16a562f282e4e4dc31bb85c2
Haroune Hamenniche <91731030+haroune22@users.noreply.github.com>
2023-06-04 11:03:08 +0100

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -5,12 +5,7 @@ import { getStorage} from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOVlsFRj7GKc9_n6iLRO9NcoGp759JSA0",
-  authDomain: "chat-58184.firebaseapp.com",
-  projectId: "chat-58184",
-  storageBucket: "chat-58184.appspot.com",
-  messagingSenderId: "399045275946",
-  appId: "1:399045275946:web:2fad2bba0b68de8e816e53"
+//fireabse Config 
 };
 
 export const app = initializeApp(firebaseConfig);
